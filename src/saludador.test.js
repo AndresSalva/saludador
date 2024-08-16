dd08c30b48480b1af3b5fa1294c4dfac84588a37
AndresSalva <andres.salvatierra@ucb.edu.bo>
2024-08-16 16:38:45 -0400

    aniadiendo test plantilla

--- src/saludador.test.js
+++ src/saludador.test.js
@@ -1,2 +1,8 @@
 import {getnombre,getgenero,getedad,traducir} from "./saludador";
 
+describe('Suite de pruebas básica', () => {
+    it('debería ejecutar un test básico', () => {
+      expect(true).toBe(true);
+    });
+  });
+  
